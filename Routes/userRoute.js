6a17fbc0e08e5e82d9788fc60f8d809f847fe540
BhaveshJoshi-07 <bhaveshjoshi0730@gmail.com>
2024-10-11 17:00:54 +0530

    created User Profile

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const userRouter = express.Router();
-const {userSignUp, usersignIn} = require("../Controllers/usercontroller.js");
+const {userSignUp, usersignIn, updateUser} = require("../Controllers/usercontroller.js");
 
 userRouter.get("/signup", (req, res) => {
     res.send("Sign up route!");
 })
 
 userRouter.post("/signup", userSignUp);
+
+userRouter.patch("/:id", updateUser)
 
 userRouter.get("/signin", (req, res) => {
     res.send("sign in route!");
